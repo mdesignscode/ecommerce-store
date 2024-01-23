2d3b966f9357566a4a65181c02a2f4ff7e9bfff3
Mdesignscode <marlonbaatjes@gmail.com>
2024-01-23 18:27:44 +0200

    ➕👦👧🧩 add clerk user button ➕👦👧🧩

--- app/Components/Navigation/Mobile.tsx
+++ app/Components/Navigation/Mobile.tsx
@@ -1,27 +1,42 @@
 "use client";
 
 import { Disclosure } from "@headlessui/react";
-import {
-  Bars3Icon,
-  XMarkIcon,
-} from "@heroicons/react/24/outline";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
+import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
 
 export default function MobileNavbar() {
   return (
     <Disclosure as="nav" className="md:hidden">
       {({ open }) => (
         <>
-          <section className="flex justify-between p-2 items-center text-light bg-dark">
-            <Link href="/" className="text-xl font-bold">Dev Store.</Link>
-
-            <Disclosure.Button>
-              {open ? (
-                <XMarkIcon className="h-12 w-12" aria-hidden="true" />
-              ) : (
-                <Bars3Icon className="h-12 w-12" aria-hidden="true" />
-              )}
-            </Disclosure.Button>
+          <section className="flex flex-col p-2 text-light bg-dark">
+            <section className="flex justify-between items-center">
+              <Link href="/" className="text-xl font-bold">
+                Dev Store.
+              </Link>
+              <Disclosure.Button>
+                {open ? (
+                  <XMarkIcon className="h-12 w-12" aria-hidden="true" />
+                ) : (
+                  <Bars3Icon className="h-12 w-12" aria-hidden="true" />
+                )}
+              </Disclosure.Button>
+            </section>
+            {open && (
+              <section className="flex flex-col gap-2">
+                <SignedIn>
+                  {/* Mount the UserButton component */}
+                  <UserButton />
+                </SignedIn>
+                <SignedOut>
+                  <UserCircleIcon height={25} width={25} />
+                  {/* Signed out users get sign in button */}
+                  <SignInButton />
+                </SignedOut>
+              </section>
+            )}
           </section>
         </>
       )}
