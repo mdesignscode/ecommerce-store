12728a101da671491aac12a1a0863cb2b5d4785a
Mdesigns Code <marlonbaatjes62@gmail.com>
2025-06-28 15:59:15 +0200

    🪄

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../prisma/generated/client'
+import { PrismaClient } from '@prisma/client'
 
 // PrismaClient is attached to the `global` object in development to prevent
 // exhausting your database connection limit.
@@ -13,3 +13,4 @@ export const prisma = globalForPrisma.prisma || new PrismaClient()
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma
 
 export default prisma
+
